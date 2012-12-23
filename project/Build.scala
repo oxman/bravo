bb77d7840fa14a5099eb20b945e7e7ddcc08ea42
oxman <oxman@bouh.org>
2012-12-23 18:17:01 +0100

    upgrade version of libs/play

--- project/Build.scala
+++ project/Build.scala
@@ -5,14 +5,14 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "bravo"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0.1-SNAPSHOT"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
     )
 
 }
